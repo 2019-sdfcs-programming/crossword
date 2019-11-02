2ffcff1a910a2e31d98aef20d5fe13e8e2870a5c
koreanlch <49800235+koreanlch@users.noreply.github.com>
2019-11-02 11:49:24 +0900

    Add files via upload

--- code.py
+++ code.py
@@ -14,20 +14,41 @@ class MainformClass(QMainWindow, formClass):
         super().__init__()
         self.setupUi(self)
         self.B1.clicked.connect(self.b1)
+        self.B2.clicked.connect(self.b2)
         self.B11.clicked.connect(self.b11)
+        now = 0
 
-    def b1(self):
-        self.t.setPlainText('바보')
-        def b11(self):
-        E = self.e.toPlainText()
+    def initDisplay(self): 
+        self.e.setPlainText('')
+        self.label_14.setText('')
 
-        if E =='숭덕고등학교':
+    def b11(self):
+        K = self.e.toPlainText()
+        if K =='숭덕고등학교' and self.now == 1:
             self.T1.setPlainText('숭')
             self.T2.setPlainText('덕')
             self.T3.setPlainText('고')
             self.T4.setPlainText('등')
             self.T5.setPlainText('학')
             self.T6.setPlainText('교')
+        elif K == '등차수열' and self.now == 2:
+            self.T4.setPlainText('등')
+            self.T8.setPlainText('차')
+            self.T10.setPlainText('수')
+            self.T12.setPlainText('열')
+        else:
+            pass
+            
+
+    def b1(self):
+        self.now = 1
+        self.initDisplay()
+        self.t.setPlainText('바보')
+
+    def b2(self):
+        self.now = 2
+        self.initDisplay()
+        self.t.setPlainText('설명')
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
